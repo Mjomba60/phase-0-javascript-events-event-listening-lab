f3042741193804587b92f7022275df35def5262a
George Samuel <geosam2708@gmail.com>
2023-03-22 21:50:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const input = document.getElementById('button')
-input.addEventListener('click', function(){
-    return alert('I was clicked')
-})
+function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+        return alert('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert);
+}
+
+addingEventListener();
